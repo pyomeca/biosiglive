33897e4c41d1f7fc77381ef56a9199e191b22d1e
ceglia <amedeo.ceglia@umontreal.ca>
2022-05-25 15:38:38 -0400

    Add init for python install

---
+++ biosiglive/__init__.py
@@ -0,0 +1,15 @@
+from .gui.plot import Plot, LivePlot
+
+from .interfaces.pytrigno_interface import PytrignoClient
+from .interfaces.vicon_interface import ViconClient
+from .interfaces.client_interface import TcpClient
+from .interfaces.param import Type, Device, MarkerSet
+
+
+from .io.save_data import read_data, add_data_to_pickle
+
+from .processing.data_processing import RealTimeProcessing, OfflineProcessing, GenericProcessing
+from .processing.msk_functions import kalman_func
+
+from .streaming.client import Client
+from .streaming.connection import Server
