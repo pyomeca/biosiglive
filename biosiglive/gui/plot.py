308fd87d6463948abc7ad60fa29481d503009288
Amedeo <amedeo.ceglia@umontreal.ca>
2022-11-07 10:58:56 -0500

    Add plot buffer

--- biosiglive/gui/plot.py
+++ biosiglive/gui/plot.py
@@ -12,15 +12,59 @@ import numpy as np
 from typing import Union
 import matplotlib.pyplot as plt
 from math import ceil
+from ..enums import PlotType
 
 
 class Plot:
+    def __init__(self, nb_subplots: int, plot_type: Union[PlotType, str] = PlotType.Curve, name: str = None, channel_names: list = None, unit: str = None):
+        """
+        Initialize the plot class.
+
+        Parameters
+        ----------
+        plot_type : Union[PlotType, str]
+            type of the plot (curve, spectrogram, ...)
+        name : str
+            name of the plot
+        channel_names : list
+            list of the channel names
+        nb_subplots : int
+            number of subplots
+        unit : str
+            unit of the plot
+        """
+        if isinstance(plot_type, str):
+            if plot_type not in [t.value for t in PlotType]:
+                raise ValueError("Plot type not recognized")
+            self.plot_type = PlotType(plot_type)
+        self.plot_type = plot_type
+
+        if nb_subplots and channel_names:
+            if len(channel_names) != nb_subplots:
+                raise ValueError("The number of subplots is not equal to the number of channel names")
+
+        self.channel_names = channel_names
+        self.figure_name = name
+        self.nb_subplot = nb_subplots
+        self.unit = unit
+
+
+class OfflinePlot:
     """
-    This class is used to plot the data offline.
+    This class is used to plot data offline.
     """
-    def multi_plot(self, data: Union[list, np.ndarray], x: Union[list, np.ndarray] = None,
-                   nb_column: int = None, y_label: str = None, x_label: str = None,
-                   legend: Union[list, str] = None, subplot_title: Union[str, list] = None, figure_name: str = None):
+
+    @staticmethod
+    def multi_plot(
+        data: Union[list, np.ndarray],
+        x: Union[list, np.ndarray] = None,
+        nb_column: int = None,
+        y_label: str = None,
+        x_label: str = None,
+        legend: Union[list, str] = None,
+        subplot_title: Union[str, list] = None,
+        figure_name: str = None,
+    ):
         """
         This function is used to plot multiple data in one figure.
         Parameters
@@ -65,72 +109,72 @@ class Plot:
             plt.legend()
             if subplot_title:
                 plt.title(subplot_title[i], fontsize=size_police)
-        # plt.tight_layout()
         plt.show()
-
-# TODO : create a class for the plot object
 
 
 class LivePlot:
     """
     This class is used to plot the data in live mode.
     """
-    def __init__(self, multi_process: bool = False):
+
+    def __init__(self):
         """
         This function is used to initialize the LivePlot class.
-        Parameters
-        ----------
-        multi_process: bool
-            If True, the plot is done in a separate process.
         """
-        self.multi_process = multi_process
         self.check_box = True
-        self.plot = []
+        self.plots = []
         self.resize = (800, 800)
         self.move = (0, 0)
         self.progress_bar_max = 1000
-        self.type = "curve"
         self.msk_model = None
+        self.plots_buffer = []
+        self.plots_windows = []
 
-    def add_new_plot(self, plot_name: str = "qt_app",
-                     plot_type="curve",
-                     channel_names: Union[str, list] = None,
-                     nb_subplot: int = None,
-                     unit: str = ""):
+    # TODO: change plot type by PlotType enum
+    def add_new_plot(
+        self,
+        nb_subplot: int,
+        plot_name: str = "qt_app",
+        plot_type: Union[PlotType, str] = PlotType.Curve,
+        channel_names: Union[str, list] = None,
+        unit: str = "",
+    ):
         """
         This function is used to add a new plot.
         Parameters
         ----------
+        nb_subplot: int
+            The number of subplot in the plot.
         plot_name: str
             The name of the plot.
         plot_type: str
             The type of the plot.
         channel_names: str or list
             The name of the channels.
+        unit: str
+            The unit of the plot.
         """
-        if not nb_subplot and not channel_names and plot_type != "skeleton":
-            raise RuntimeError("you must defined a number of subplot or a list of a chanel names")
+        for plot in self.plots:
+            if plot.figure_name == plot_name:
+                if plot.figure_name[-1] != "1":
+                    try:
+                        int(plot.figure_name[-1])
+                        plot_name = plot.figure_name[:-1] + str(int(plot.figure_name[-1]) + 1)
+                    except ValueError:
+                        plot_name = plot_name + "_1"
+        self.plots.append(Plot(nb_subplot, plot_type, plot_name, channel_names, unit))
+        self.plots_windows.append(None)
+        self.plots_buffer.append(None)
 
-        if nb_subplot and channel_names:
-            if nb_subplot != len(channel_names):
-                raise RuntimeError(f"You have defined a number of subplot ({nb_subplot} different"
-                                   f" than the length of the chanel names {len(channel_names)}.")
-
-        plot = LivePlot()
-        plot.type = plot_type
-        plot.channel_names = channel_names
-        plot.figure_name = plot_name
-        plot.nb_subplot = nb_subplot
-        plot.unit = unit
-        self.plot.append(plot)
-
-    def init_plot_window(self, plot, use_checkbox: bool = True, remote: bool = True):
+    def init_plot(self, plot_name, plot_windows: int=None, use_checkbox: bool = True, remote: bool = True, **kwargs):
         """
         This function is used to initialize the qt app.
         Parameters
         ----------
-        plot: method
+        plot_name: str
             The plot to initialize.
+        plot_windows: int
+            The number of plot windows.
         use_checkbox: bool
             If True, the checkbox is used.
         remote: bool
@@ -147,31 +191,33 @@ class LivePlot:
         box: QCheckBox
             The checkbox.
         """
-        if plot.type == "curve":
-            rplt, layout, app, box = self._init_curve(plot.figure_name,
-                                                      plot.channel_names,
-                                                      plot.nb_subplot,
-                                                      checkbox=use_checkbox,
-                                                      remote=remote)
+        plot_idx = [i for i, plot in enumerate(self.plots) if plot.figure_name == plot_name][0]
+        plot = self.plots[plot_idx]
+        self.plots_windows[plot_idx] = plot_windows
+        if plot.type == PlotType.Curve:
+            rplt, layout, app, box = self._init_curve(
+                plot.figure_name, plot.channel_names, plot.nb_subplot, checkbox=use_checkbox
+            )
             return rplt, layout, app, box
-        elif plot.type == "progress_bar":
-            rplt, layout, app = self._init_progress_bar(plot.figure_name,
-                                                        plot.nb_subplot,
-                                                        )
+        elif plot.type == PlotType.ProgressBar:
+            rplt, layout, app = self._init_progress_bar(
+                plot.figure_name,
+                plot.nb_subplot,
+            )
             return rplt, layout, app
 
-        if plot.type == "skeleton":
-            plot.viz = self._init_skeleton(self.msk_model)
+        if plot.type == PlotType.Skeleton:
+            plot.viz = self._init_skeleton(self.msk_model, **kwargs)
 
         else:
             raise ValueError(f"The plot type ({plot.type}) is not supported.")
 
-    def update_plot_window(self, plot, data: np.ndarray, app=None, rplt=None, box=None):
+    def update_plot(self, plot_name, data: np.ndarray, app=None, rplt=None, box=None):
         """
         This function is used to update the qt app.
         Parameters
         ----------
-        plot: method
+        plot_name: str
             The plot to update.
         data: np.ndarray
             The data to plot.
@@ -182,6 +228,16 @@ class LivePlot:
         box: QCheckBox
             The checkbox.
         """
+        plot_idx = [i for i, plot in enumerate(self.plots) if plot.figure_name == plot_name][0]
+        plot = self.plots[plot_idx]
+        if self.plots_windows[plot_idx]:
+            if self.plots_buffer[plot_idx]:
+                if self.plots_buffer[plot_idx].shape[1] < self.plots_windows[plot_idx]:
+                    self.plots_buffer[plot_idx] = np.append((self.plots_buffer[plot_idx]), data, axis=1)
+                elif self.plots_buffer[plot_idx].shape[1] >= self.plots_windows[plot_idx]:
+                    size = data.shape[1]
+                    self.plots_buffer[plot_idx] = np.append((self.plots_buffer[plot_idx, size:]), data, axis=1)
+                data = self.plots_buffer[plot_idx]
 
         if plot.type == "progress_bar":
             self._update_progress_bar(data, app, rplt, plot.channel_names, plot.unit)
@@ -192,11 +248,14 @@ class LivePlot:
         else:
             raise ValueError(f"The plot type ({plot.type}) is not supported.")
 
-    def _init_curve(self, figure_name: str = "Figure",
-                    subplot_label: Union[list, str] = None,
-                    nb_subplot: int = None,
-                    checkbox: bool = True,
-                    remote: bool = True):
+    def _init_curve(
+        self,
+        figure_name: str = "Figure",
+        subplot_label: Union[list, str] = None,
+        nb_subplot: int = None,
+        checkbox: bool = True,
+        # remote: bool = True,
+    ):
         """
         This function is used to initialize the curve plot.
         Parameters
@@ -320,12 +379,12 @@ class LivePlot:
             if len(box) != 0:
                 if box[i].isChecked() is True:
                     if len(data.shape) == 2:
-                        rplt[i].plot(data[i, :], clear=True, _callSync='off')
+                        rplt[i].plot(data[i, :], clear=True, _callSync="off")
                     else:
                         raise ValueError("The data must be a 2D array.")
             else:
                 if len(data.shape) == 2:
-                    rplt[i].plot(data[i, :], clear=True, _callSync='off')
+                    rplt[i].plot(data[i, :], clear=True, _callSync="off")
                 else:
                     raise ValueError("The data must be a 2D array.")
         app.processEvents()
@@ -349,8 +408,10 @@ class LivePlot:
         """
 
         if subplot_label and len(subplot_label) != data.shape[0]:
-            raise RuntimeError(f"The length of Subplot labels ({len(subplot_label)}) is different than"
-                               f" the first dimension of your data ({data.shape[0]}).")
+            raise RuntimeError(
+                f"The length of Subplot labels ({len(subplot_label)}) is different than"
+                f" the first dimension of your data ({data.shape[0]})."
+            )
 
         for i in range(data.shape[0]):
             value = np.mean(data[i, :])
@@ -363,45 +424,13 @@ class LivePlot:
     def _update_skeleton(data: np.ndarray, viz):
         viz.set_q(data, refresh_window=True)
 
-    def _init_skeleton(self, model_path: str):
-        import bioviz
-        if not self.skeleton_plot_initialized:
-            self.set_skeleton_plot_options()
-        plot = bioviz.Viz(
-            model_path=model_path,
-            show_global_center_of_mass=self.show_global_center_of_mass,
-            show_markers=self.show_markers,
-            show_floor=self.show_floor,
-            show_gravity_vector=self.show_gravity_vector,
-            show_muscles=self.show_muscles,
-            show_segments_center_of_mass=self.show_segments_center_of_mass,
-            show_local_ref_frame=self.show_local_ref_frame,
-            show_global_ref_frame=self.show_global_ref_frame,
-        )
+    def _init_skeleton(self, model_path: str, **kwargs):
+        try:
+            import bioviz
+        except ImportError:
+            raise ImportError("Please install bioviz (github.com/pyomeca/bioviz) to use the skeleton plot.")
+        plot = bioviz.Viz(model_path, **kwargs)
         return plot
-
-    def set_skeleton_plot_options(self,
-                                  show_global_center_of_mass=False,
-                                  show_markers=True,
-                                  show_floor=False,
-                                  show_gravity_vector=False,
-                                  show_muscles=False,
-                                  show_segments_center_of_mass=False,
-                                  show_local_ref_frame=False,
-                                  show_global_ref_frame=False
-                                  ):
-
-        self.show_global_center_of_mass = show_global_center_of_mass
-        self.show_markers = show_markers
-        self.show_floor = show_floor
-        self.show_gravity_vector = show_gravity_vector
-        self.show_muscles = show_muscles
-        self.show_segments_center_of_mass = show_segments_center_of_mass
-        self.show_local_ref_frame = show_local_ref_frame
-        self.show_global_ref_frame = show_global_ref_frame
-        self.skeleton_plot_initialized = True
-
-
 
     @staticmethod
     def _init_layout(figure_name: str = "Figure", resize: tuple = (400, 400), move: tuple = (0, 0)):
