48589808800e570ccd1a81bdcf7ce88ca3c0d2e7
Amedeo <amedeo.ceglia@umontreal.ca>
2022-11-07 11:00:11 -0500

    Add enum for params

--- biosiglive/interfaces/param.py
+++ biosiglive/interfaces/param.py
@@ -2,43 +2,52 @@
 This file is part of biosiglive. It contains the Parameter class and introduce the device and markers classes.
 """
 from math import ceil
-from ..processing.data_processing import RealTimeProcessing, OfflineProcessing, GenericProcessing
-from ..processing.msk_functions import kalman_func
+from ..enums import DeviceType, MarkerType
+from typing import Union
 
 
-class Type:
-    def __init__(self, name: str = None, type: str = None, rate: float = None, system_rate: float = 100, real_time: bool = False):
+class Param:
+    def __init__(
+        self,
+        param_type: Union[DeviceType, MarkerType],
+        nb_channels: int,
+        name: str = None,
+        rate: float = None,
+        system_rate: float = 100,
+    ):
         """
         initialize the parameter class
 
         Parameters
         ----------
+        param_type : Union[DeviceType, MarkerType]
+            enum type of the parameter (emg, imu, markers, ...)
+        nb_channels : int
+            number of channels of the parameter
         name : str
             name of the parameter
-        type : str
-            type of the parameter (emg, imu, markers, ...)
         rate : float
             rate of the parameter
         system_rate : float
             rate of the system
-        real_time : bool
-            If true device wil be use in real time application
         """
-
+        self.nb_channels = nb_channels
         self.name = name
-        self.type = type
+        self.param_type = param_type
         self.rate = rate
         self.system_rate = system_rate
         self.sample = ceil(rate / self.system_rate)
         self.range = None
         self.process_method = None
-        # self._set_process_method(real_time)
 
     def set_name(self, name: str):
         self.name = name
 
-    def set_type(self, type: str):
-        self.type = type
+    def set_type(self, param_type: Union[DeviceType, MarkerType]):
+        self.param_type = param_type
+
+    def get_type(self):
+        return self.param_type
 
     def set_rate(self, rate: int):
         self.rate = rate
@@ -50,15 +59,29 @@ class Type:
         self.process_method = processing_class
 
 
-class Device(Type):
+class Device(Param):
     """
     This class is used to store the available devices.
     """
 
-    def __init__(self, name: str = None, type: str = "emg", rate: float = 2000, system_rate: float = 100, channel_names: list = None):
-        super().__init__(name, type, rate, system_rate)
+    def __init__(
+        self,
+        device_type: DeviceType = DeviceType.Emg,
+        nb_channels: int = 1,
+        name: str = None,
+        rate: float = 2000,
+        system_rate: float = 100,
+        channel_names: Union[list, str] = None,
+    ):
+        super().__init__(device_type, nb_channels, name,  rate, system_rate)
+        if isinstance(channel_names, str):
+            channel_names = [channel_names]
+        if nb_channels != len(channel_names):
+            raise ValueError("The number of channels is not equal to the number of channel names.")
+        self.device_range = None
         self.infos = None
         self.channel_names = channel_names
+        self.interface = None
 
     def add_channel_names(self, channel_names: list):
         """
@@ -71,21 +94,18 @@ class Device(Type):
         self.channel_names = channel_names
 
 
-# class Imu(Type):
-#     """
-#     This class is used to store the available IMU devices.
-#     """
-#     def __init__(self, name: str = None, rate: float = None, from_emg: bool = False):
-#         type = "imu" if not from_emg else "imu_from_emg"
-#         super().__init__(name, type, rate)
-
-
-class MarkerSet(Type):
+class MarkerSet(Param):
     """
     This class is used to store the available markers.
     """
-    def __init__(self, name: str = None, rate: float = None, unlabeled: bool = False, recons_kin: bool = False):
-        type = "unlabeled" if unlabeled else "labeled"
-        super().__init__(name, type, rate)
-        self.markers_names = name
+
+    def __init__(self, nb_channels: int = 1, name: str = None, marker_names: Union[str, list] = None, rate: float = None, unlabeled: bool = False, system_rate: float = 100):
+        marker_type = MarkerType.Unlabeled if unlabeled else MarkerType.Labeled
+        super().__init__(marker_type, nb_channels, name, rate, system_rate)
+        if isinstance(marker_names, str):
+            self.marker_names = [marker_names]
+        if nb_channels != len(self.marker_names):
+            raise ValueError("The number of channels and the number of markers names are not the same.")
+        self.markers_names = marker_names
         self.subject_name = None
+        self.interface = None
