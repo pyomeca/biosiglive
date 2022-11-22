3aac34b351d72bc825c03edb0fb18f35ef49fe9b
Amedeo <amedeo.ceglia@umontreal.ca>
2022-11-22 18:34:00 -0500

    Add dic merger

--- biosiglive/streaming/utils.py
+++ biosiglive/streaming/utils.py
@@ -29,3 +29,21 @@ def get_data_from_file(data_type: list, file_path: str):
 
 def adjust_dim_data(data: np.ndarray, data_type: list, data_shape: list):
     return data
+
+
+def dic_merger(dic_to_merge, new_dic=None):
+    if not new_dic:
+        new_dic = dic_to_merge
+    else:
+        for key in dic_to_merge.keys():
+            if isinstance(dic_to_merge[key], dict):
+                new_dic[key] = dic_merger(dic_to_merge[key], new_dic[key])
+            elif isinstance(dic_to_merge[key], list):
+                new_dic[key] = dic_to_merge[key] + new_dic[key]
+            elif isinstance(dic_to_merge[key], np.ndarray):
+                new_dic[key] = np.append(dic_to_merge[key], new_dic[key],  axis=0)
+            elif isinstance(dic_to_merge[key], int):
+                if isinstance(new_dic[key], int):
+                    new_dic[key] = [new_dic[key]]
+                new_dic[key] = [new_dic[key]] + [dic_to_merge[key]]
+    return new_dic
